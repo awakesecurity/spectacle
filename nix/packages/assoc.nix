e60153ed8bdbad6dfaf7e5a2e6f71f44d80ad8d1
Evan Relf <evan@awakesecurity.com>
2021-03-26 12:47:42 -0700

    Stop pinning Haskell packages

--- nix/packages/assoc.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, base, bifunctors, lib, tagged }:
-mkDerivation {
-  pname = "assoc";
-  version = "1.0.2";
-  sha256 = "d8988dc6e8718c7a3456515b769c9336aeeec730cf86fc5175247969ff8f144f";
-  libraryHaskellDepends = [ base bifunctors tagged ];
-  description = "swap and assoc: Symmetric and Semigroupy Bifunctors";
-  license = lib.licenses.bsd3;
-}
--- release.nix
+++ release.nix
@@ -13,63 +13,13 @@ let
       in
     builtins.listToAttrs (map toKeyVal haskellPaths);
 
-
   overlays = [
     (newPkgs: oldPkgs: rec {
       haskellPackages = oldPkgs.haskellPackages.extend (
         newPkgs.lib.fold newPkgs.lib.composeExtensions (_: _: {}) [
-          (readDirectory ./nix/packages)
           (haskellPackagesNew: haskellPackagesOld: {
             spectacle = haskellPackagesNew.callCabal2nix "spectacle" ./. { };
-
-            # Infinite recursion since quickcheck depends on splitmix
-            splitmix = newPkgs.haskell.lib.dontCheck haskellPackagesOld.splitmix;
-
-            # HsYAML-aeson is no longer broken
-            HsYAML-aeson = newPkgs.haskell.lib.unmarkBroken haskellPackagesOld.HsYAML-aeson;
-
-            HsYAML = newPkgs.haskell.lib.dontCheck haskellPackagesOld.HsYAML;
-
-            cassava = newPkgs.haskell.lib.dontCheck haskellPackagesOld.cassava;
-
-            # Diff's test suite fails to compile
-            Diff = newPkgs.haskell.lib.dontCheck haskellPackagesOld.Diff;
-
-            ghc-lib-parser = newPkgs.haskell.lib.dontHaddock haskellPackagesOld.ghc-lib-parser;
-
-            ghc-lib-parser_8_8_2_20200205 = null;
-            ghc-lib-parser-ex_8_8_4_0 = null;
-
-            primitive = newPkgs.haskell.lib.dontCheck haskellPackagesOld.primitive;
-
-            system-fileio = newPkgs.haskell.lib.dontCheck haskellPackagesOld.system-fileio;
-
-            psqueues = newPkgs.haskell.lib.dontCheck haskellPackagesOld.psqueues;
-
-            HTTP = newPkgs.haskell.lib.dontCheck haskellPackagesOld.HTTP;
-
-            ed25519 = newPkgs.haskell.lib.dontCheck haskellPackagesOld.ed25519;
-
-            lukko = newPkgs.haskell.lib.dontCheck haskellPackagesOld.lukko;
-
-            hackage-security = newPkgs.haskell.lib.dontCheck haskellPackagesOld.hackage-security;
-
-            cryptohash-sha256 =
-              let
-                pkg = haskellPackagesNew.callPackage ./nix/packages/cryptohash-sha256.nix { cryptohash-sha256-pure = null; };
-              in
-                newPkgs.haskell.lib.enableCabalFlag (newPkgs.haskell.lib.dontCheck pkg) "use-cbits";
-
-            cryptohash-sha512 = newPkgs.haskell.lib.dontCheck haskellPackagesOld.cryptohash-sha512;
-
-            # From https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/haskell-modules/configuration-ghc-8.10.x.nix#L80
-            language-haskell-extract = newPkgs.haskell.lib.appendPatch (newPkgs.haskell.lib.doJailbreak haskellPackagesOld.language-haskell-extract) (newPkgs.fetchpatch {
-              name = "language-haskell-extract-0.2.4.patch";
-              url = "https://gitlab.haskell.org/ghc/head.hackage/-/raw/e48738ee1be774507887a90a0d67ad1319456afc/patches/language-haskell-extract-0.2.4.patch?inline=false";
-              sha256 = "0rgzrq0513nlc1vw7nw4km4bcwn4ivxcgi33jly4a7n3c1r32v1f";
-            });
           })
-
         ]
       );
     })
