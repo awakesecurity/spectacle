6236b8d5424e0a90918818a65be503858e683bf7
Jake <jacob.leach@arista.com>
2021-04-21 12:57:26 -0500

    change: fix type inference for extensible records projections.
    Changes:
    - Fixes type inference on Rec projections so that variable usage is unambiguous
    - Simplifies the Rec interface to remove the 'Field' type.
    - Remove unused declarations from 'Data.Name'.
    - Relocated 'Rec' module under 'Data.Type'.

---
+++ src/Data/Ascript.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Data.Ascript
+  ( Ascribe (..),
+    type (#),
+    type AscriptName,
+    type AscriptType,
+  )
+where
+
+import Data.Kind (Type)
+import GHC.TypeLits (Symbol)
+
+-- -------------------------------------------------------------------------------------------------
+
+-- | Type ascriptions.
+--
+-- @since 0.1.0.0
+data Ascribe s a = Ascribe s a
+
+-- | The kind of type ascribed variables in the type row of a 'Data.Type.Rec'.
+--
+-- @since 0.1.0.0
+infix 6 #
+
+type (#) :: Symbol -> Type -> Ascribe Symbol Type
+type s # a = 'Ascribe s a
+
+-- | Project the type of a 'Data.Type.Rec' ascription.
+--
+-- @since 0.1.0.0
+type AscriptType :: Ascribe Symbol Type -> Type
+type family AscriptType a where
+  AscriptType (_ # a) = a
+
+-- | Project the symbol of a 'Data.Type.Rec' ascription.
+--
+-- @since 0.1.0.0
+type AscriptName :: Ascribe Symbol Type -> Symbol
+type family AscriptName a where
+  AscriptName (s # _) = s
