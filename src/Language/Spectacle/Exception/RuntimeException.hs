ed66cb0fbefe86bf58a07cf9cace1c9c89e4e09c
Jake <jacob.leach@arista.com>
2021-04-22 12:06:10 -0500

    change: quantifiers to be a higher-order effect

--- src/Language/Spectacle/Exception/RuntimeException.hs
+++ src/Language/Spectacle/Exception/RuntimeException.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE DeriveAnyClass #-}
 
 module Language.Spectacle.Exception.RuntimeException
-  ( RuntimeException (VariableException),
+  ( RuntimeException (VariableException, QuantifierException),
     VariableException (CyclicReference),
+    QuantifierException (ForallViolated, ExistsViolated)
   )
 where
 
@@ -13,10 +14,17 @@ import Type.Reflection (Typeable)
 
 data RuntimeException where
   VariableException :: VariableException -> RuntimeException
+  QuantifierException :: QuantifierException -> RuntimeException
   deriving stock (Show, Typeable)
   deriving anyclass Exception
 
 data VariableException where
   CyclicReference :: [String] -> VariableException
+  deriving stock (Show, Typeable)
+  deriving anyclass Exception
+
+data QuantifierException where
+  ForallViolated :: QuantifierException
+  ExistsViolated :: QuantifierException
   deriving stock (Show, Typeable)
   deriving anyclass Exception
---
+++ src/Language/Spectacle/Syntax/Quantify/Internal.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Language.Spectacle.Syntax.Quantify.Internal
+  ( Quantify (Quantify),
+    Effect (Forall, Exists),
+  )
+where
+
+import Data.Void (Void)
+
+import Language.Spectacle.Exception.RuntimeException
+import Language.Spectacle.Lang (Effect, EffectK, Lang, Members, ScopeK)
+import Language.Spectacle.Syntax.Error.Internal (Error)
+import Language.Spectacle.Syntax.NonDet.Internal (NonDet)
+
+-- -------------------------------------------------------------------------------------------------
+
+newtype Quantify :: EffectK where
+  Quantify :: Void -> Quantify a
+
+data instance Effect Quantify :: ScopeK where
+  Forall ::
+    (Members '[Error RuntimeException, NonDet] effs, m ~ Lang ctx effs) =>
+    [a] ->
+    (a -> m Bool) ->
+    Effect Quantify m a
+  Exists ::
+    (Members '[Error RuntimeException, NonDet] effs, m ~ Lang ctx effs) =>
+    [a] ->
+    (a -> m Bool) ->
+    Effect Quantify m a
