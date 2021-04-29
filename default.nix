9298a38d75dfdd9f8cc7956c1699d4256f09d082
Evan Relf <evan@awakesecurity.com>
2021-04-29 12:15:36 -0700

    Move shell code to `shell.nix`

--- default.nix
+++ default.nix
@@ -1,17 +1,9 @@
 let
   pkgs = import ./nix/pkgs.nix;
 
-  spectacle-shell = (pkgs.haskellPackages.shellFor {
-    packages = p: [
-      p.spectacle
-    ];
-    buildInputs = [ pkgs.haskellPackages.cabal-install pkgs.haskellPackages.ghcid ];
-  });
-
 in {
   inherit pkgs;
   inherit (pkgs.haskellPackages) fourmolu dhall-yaml dhall-nix;
-  inherit spectacle-shell;
 
   spectacle =
     pkgs.haskell.lib.appendConfigureFlags pkgs.haskellPackages.spectacle [
--- shell.nix
+++ shell.nix
@@ -1,1 +1,12 @@
-(import ./default.nix).spectacle-shell
+let
+  pkgs = import ./nix/pkgs.nix;
+
+  spectacle = pkgs.haskellPackages.spectacle;
+
+in
+spectacle.env.overrideAttrs (old: {
+  buildInputs = [
+    pkgs.cabal-install
+    pkgs.ghcid
+  ];
+})
