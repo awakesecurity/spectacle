60389013fd4463889c7d92966430678298fc810a
Evan Relf <evan@awakesecurity.com>
2021-04-29 12:18:10 -0700

    Stop sourcing packages from `default.nix`
    We can just get them from the package set directly, no middle man
    boilerplate required.

--- default.nix
+++ default.nix
@@ -2,9 +2,6 @@ let
   pkgs = import ./nix/pkgs.nix;
 
 in {
-  inherit pkgs;
-  inherit (pkgs.haskellPackages) fourmolu dhall-yaml dhall-nix;
-
   spectacle =
     pkgs.haskell.lib.appendConfigureFlags pkgs.haskellPackages.spectacle [
       "--ghc-options=-Werror"
