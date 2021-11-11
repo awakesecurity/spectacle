c696f754f2b737413712a0ff09428ed68556b198
Jake <jacob.leach@arista.com>
2021-11-11 09:42:13 -0600

    add: queue distribution

--- src/Control/Applicative/Phases.hs
+++ src/Control/Applicative/Phases.hs
@@ -9,6 +9,7 @@
 -- @since 0.1.0.0
 module Control.Applicative.Phases
   ( Phases (Here, There),
+    hoist,
     lower,
     wrap,
     lift,
@@ -16,9 +17,9 @@ module Control.Applicative.Phases
   )
 where
 
-import Control.Applicative ( Applicative(liftA2) )
+import Control.Applicative (Applicative (liftA2))
+import Control.Monad.Zip (MonadZip, mzipWith)
 import Data.Kind (Type)
-import Control.Monad.Zip
 
 -- ---------------------------------------------------------------------------------------------------------------------
 
@@ -34,32 +35,10 @@ data Phases :: (Type -> Type) -> Type -> Type where
   Here :: a -> Phases f a
   There :: (a -> b -> c) -> f a -> Phases f b -> Phases f c
 
-lower :: Applicative f => Phases f a -> f a
-lower (Here x) = pure x
-lower (There op x xs) = liftA2 op x (lower xs)
-
-wrap :: Applicative f => Phases f a -> Phases f a
-wrap = There (\_ xs -> xs) (pure ())
-
-lift :: Monad f => f (Phases f a) -> Phases f a
-lift f = There const (f >>= lower) (pure ())
-
--- | Tensoring 'Phases' over 'MonadZip', preserves effect layering.
---
--- @since 0.1.0.0
-tensor :: (MonadZip g, Monoid a, Applicative f) => Phases f (g a) -> Phases f (g a) -> Phases f (g a)
-tensor (Here x) (Here y) = Here (mzipWith (<>) x y)
-tensor (There f x xs) (Here y) = There (\z zs -> mzipWith (<>) (f z zs) y) x xs
-tensor (Here x) (There g y ys) = There (\z zs -> mzipWith (<>) x (g z zs)) y ys
-tensor (There f x xs) (There g y ys) =
-  let here = liftA2 (,) x y
-      there = liftA2 (,) xs ys
-   in There (\(z, w) (zs, ws) -> mzipWith (<>) (f z zs) (g w ws)) here there
-
 -- | @since 0.1.0.0
 instance Functor (Phases f) where
   fmap f (Here x) = Here (f x)
-  fmap f (There op x xs) = There (\y ys -> f (y `op` ys)) x xs
+  fmap f (There k x xs) = There (\y ys -> f (k y ys)) x xs
   {-# INLINE fmap #-}
 
 -- | @since 0.1.0.0
@@ -73,3 +52,28 @@ instance Applicative f => Applicative (Phases f) where
     There (\(x', y') (xs', ys') -> c (f x' xs') (g y' ys')) (liftA2 (,) x y) (liftA2 (,) xs ys)
   {-# INLINE liftA2 #-}
 
+hoist :: (forall x. f x -> g x) -> Phases f a -> Phases g a
+hoist _ (Here x) = Here x
+hoist eta (There f x xs) = There f (eta x) (hoist eta xs)
+
+lower :: Applicative f => Phases f a -> f a
+lower (Here x) = pure x
+lower (There op x xs) = liftA2 op x (lower xs)
+
+wrap :: Applicative f => Phases f a -> Phases f a
+wrap = There (const id) (pure ())
+
+lift :: Monad f => f (Phases f a) -> Phases f a
+lift f = There const (f >>= lower) (pure ())
+
+-- | Tensoring 'Phases' over 'MonadZip', preserves effect layering.
+--
+-- @since 0.1.0.0
+tensor :: (MonadZip g, Monoid a, Applicative f) => Phases f (g a) -> Phases f (g a) -> Phases f (g a)
+tensor (Here x) (Here y) = Here (mzipWith (<>) x y)
+tensor (Here x) (There g y ys) = There (\z zs -> mzipWith (<>) x (g z zs)) y ys
+tensor (There f x xs) (Here y) = There (\z zs -> mzipWith (<>) (f z zs) y) x xs
+tensor (There f x xs) (There g y ys) =
+  let here = liftA2 (,) x y
+      there = liftA2 (,) xs ys
+   in There (\(z, w) (zs, ws) -> mzipWith (<>) (f z zs) (g w ws)) here there
--- src/Control/Applicative/Queue.hs
+++ src/Control/Applicative/Queue.hs
@@ -1,56 +1,95 @@
+{-# LANGUAGE TupleSections #-}
+
 -- | Effect queues.
 --
 -- @since 0.1.0.0
 module Control.Applicative.Queue
-  ( Queue,
+  ( Queue (QU),
+    pattern Queue,
+    getQU,
     runQueue,
-    wrap,
-    joinQueue,
-    tensor,
+    getQueue,
+    distribQueue,
+    wrapQueue,
+    cofreeFreeQueue,
+    fixQ,
     now,
     later,
   )
 where
 
-import Control.Applicative (Applicative (liftA2))
+import Control.Applicative (Alternative, Applicative, liftA2)
 import Control.Monad.Zip (MonadZip)
+import Data.Bifunctor
+import Data.Kind (Type)
 
 import Control.Applicative.Day (Day (Day), getDay)
-import Control.Applicative.Phases (Phases (Here, There), lower)
+import Control.Applicative.Phases (Phases (Here, There), hoist, lower)
 import qualified Control.Applicative.Phases as Phases
+import Control.Comonad.Cofree
+import Control.Monad.Free.Ap
 
 -- ---------------------------------------------------------------------------------------------------------------------
 
-type Queue f = Day (Phases f)
-
-runQueue :: Applicative f => Queue f a -> f a
-runQueue = fmap fst . Phases.lower . flip getDay (Here ())
-
-wrap :: Monad f => f (Queue f a) -> Queue f a
-wrap f = Day \x -> Phases.lift (fmap (($ x) . getDay) f)
-
-joinQueue :: Monad f => Queue f (Queue f a) -> Queue f a
-joinQueue (Day f) = Day \x ->
-  let x' = lower (fmap fst (f x))
-   in getDay (wrap x') x
-
--- | Tensoring effect queues. Left-biased on the quantified 'Day' variable so beware.
+-- | Effect queues.
 --
 -- @since 0.1.0.0
-tensor :: (Applicative f, MonadZip g, Monoid a) => Queue f (g a) -> Queue f (g a) -> Queue f (g a)
-tensor (Day f) (Day g) = Day \x ->
-  let here = Phases.tensor (fmap fst (f x)) (fmap fst (g x))
-      there = fmap snd (f x)
-   in liftA2 (,) here there
+newtype Queue :: (Type -> Type) -> Type -> Type where
+  -- TODO: prove that 'Queue' is a (free) monad.
+  QU :: {getQU :: Day (Phases f) a} -> Queue f a
+  deriving (Functor)
+
+pattern Queue :: (forall x. Phases f x -> Phases f (a, x)) -> Queue f a
+pattern Queue {getQueue} = QU (Day getQueue)
+{-# COMPLETE Queue #-}
+
+-- | @since 0.1.0.0
+instance (Applicative f, MonadZip g, Monoid a) => Semigroup (Queue f (g a)) where
+  Queue f <> Queue g = Queue \x ->
+    let here = Phases.tensor (fmap fst (f x)) (fmap fst (g x))
+        there = fmap snd (f x)
+     in liftA2 (,) here there
+
+-- | @since 0.1.0.0
+instance Applicative f => Applicative (Queue f) where
+  pure x = Queue (fmap (x,))
+  {-# INLINE pure #-}
+
+  Queue f <*> Queue g = Queue \x ->
+    let fx = f x
+        gx = g (fmap snd fx)
+     in liftA2 (,) (fmap fst fx <*> fmap fst gx) (fmap snd gx)
+  {-# INLINE (<*>) #-}
+
+runQueue :: Applicative f => Queue f a -> f a
+runQueue = fmap fst . Phases.lower . flip getQueue (Here ())
+
+fixQ :: Applicative f => (a -> Queue f a) -> Queue f a
+fixQ f = Queue (getQueue (later (fixQ f)))
+
+distribQueue :: Applicative f => f (Queue f a) -> Queue f (f a)
+distribQueue ma = Queue \mx ->
+  let mb = fmap (lower . fmap fst . (`getQueue` mx)) ma
+   in case mx of
+        Here x -> There (,) mb mx
+        There f x xs -> There (\(e, a) b -> (e, f a b)) (liftA2 (,) mb x) xs
+
+wrapQueue :: Monad f => f (Queue f a) -> Queue f a
+wrapQueue f = Queue \x -> Phases.lift (fmap (($ x) . getQueue) f)
+
+cofreeFreeQueue :: (Alternative f, Monad f) => Queue (Free f) a -> Queue f (Cofree f a)
+cofreeFreeQueue (Queue f) = Queue \x -> case f (hoist liftF x) of
+  Here (y, k) -> Here (pure y, k)
+  There g y ys -> There (\a b -> first pure (g a b)) (retract y) (hoist retract ys)
 
 now :: Applicative f => f a -> Queue f a
-now xs = Day \case
+now xs = Queue \case
   Here x -> There (,) xs (Here x)
-  There f y ys -> There (\(x, y) z -> (x, f y z)) (liftA2 (,) xs y) ys
+  There f y ys -> There (\(a, b) c -> (a, f b c)) (liftA2 (,) xs y) ys
 
 later :: Applicative f => Queue f a -> Queue f a
-later xs = Day (delay xs)
+later q = Queue (delay q)
   where
     delay :: Applicative f => Queue f a -> Phases f b -> Phases f (a, b)
-    delay (Day f) (Here y) = There (const id) (pure ()) (f (Here y))
-    delay (Day f) (There g y ys) = There (fmap . g) y (f ys)
+    delay (Queue f) (Here y) = There (const id) (pure ()) (f (Here y))
+    delay (Queue f) (There g y ys) = There (fmap . g) y (f ys)
